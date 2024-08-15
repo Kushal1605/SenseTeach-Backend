9337687dc3e33cc604ef4af04b0798fc0b1c14b8
Kushal1605 <kushalgupta1605@gmail.com>
2024-08-15 21:04:14 +0530

    nodemailer

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -1,14 +1,11 @@
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  // service: process.env.NODEMAILER_SERVICE,
-  service: 'gmail',
+  service: process.env.NODEMAILER_SERVICE,
   port: 587,
   secure: false,
   auth: {
-    // user: process.env.NODEMAILER_USER,
-    // pass: process.env.NODEMAILER_PASS,
-    user: "kushalgupta1605@gmail.com",
-    pass: "ubaq tzjk huxm yvjw",
+    user: process.env.NODEMAILER_USER,
+    pass: process.env.NODEMAILER_PASS,
   },
 });

02ae9f9193b4af2e7df2415354d0c99c500037e7
Kushal1605 <kushalgupta1605@gmail.com>
2024-08-19 20:17:32 +0530

    mongodb uri

--- src/config/db.js
+++ src/config/db.js
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    // const connection = await mongoose.connect(`${process.env.MONGODB_URI}`);
-    const connection = await mongoose.connect("mongodb://127.0.0.1/senseteach");
+    const connection = await mongoose.connect(`${process.env.MONGODB_URI}`);
     console.log(`MongoDB Connected: ${connection.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
